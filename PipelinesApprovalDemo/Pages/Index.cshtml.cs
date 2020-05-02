7910bf7d60edd87aaa2f9a545c7402073ba06db5
Jason Specland <jaspecla@microsoft.com>
2020-05-02 17:54:49 -0400

    Add project files.

---
+++ PipelinesApprovalDemo/Pages/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace PipelinesApprovalDemo.Pages
+{
+  public class IndexModel : PageModel
+  {
+    private readonly ILogger<IndexModel> _logger;
+    public string MyEnvironment;
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+      _logger = logger;
+    }
+
+    public void OnGet()
+    {
+      MyEnvironment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+      if (String.IsNullOrEmpty(MyEnvironment))
+      {
+        MyEnvironment = "(unknown)";
+      }
+
+    }
+  }
+}
